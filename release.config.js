1008d999475ae81417464345aad3b900a799244b
Nishchal Gautam <gautam.nishchal@gmail.com>
2019-08-27 22:15:50 +0700

    fix(*): fix semantic-release

--- release.config.js
+++ release.config.js
@@ -40,11 +40,11 @@ module.exports = {
         },
         {
             path: "@semantic-release/exec",
-            cmd: "docker pull fossapps/micro.starter:`git rev-parse --short=4 ${TRAVIS_COMMIT}`"
+            cmd: `docker pull fossapps/micro.starter:\`git rev-parse --short=4 ${process.env.TRAVIS_COMMIT}\``
         },
         {
             path: "@semantic-release/exec",
-            cmd: "docker tag fossapps/micro.starter:`git rev-parse --short=4 ${TRAVIS_COMMIT}` fossapps/micro.starter:${nextRelease.version}"
+            cmd: `docker tag fossapps/micro.starter:\`git rev-parse --short=4 ${process.env.TRAVIS_COMMIT}\` fossapps/micro.starter:\${nextRelease.version}`
         }
     ],
     publish: [
